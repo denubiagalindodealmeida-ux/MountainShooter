599538569709124b989331626c2b82be9e8bee8c
Denúbia Almeida <denubiagalindodealmeida@gmail.com>
2026-05-10 18:38:08 -0300

    adding lasses generated from starUML

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pygame
 print('setup start')
 pygame.init()
-window = pygame.display.set_mode(size=(600, 400))
+window = pygame.display.set_mode(size=(600, 480))
 print('setup End')
 
-print('loop Start')
+print('loop start')
 while True:
     # check for all events
-   for event in pygame.event.get():
-       if event.type ==pygame.QUIT:
-          pygame.quit() #close window
-          quit() #end pygame
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            pygame.quit() # close window
+            quit() # end pygame
+
+
+
+
+
 
 
 
