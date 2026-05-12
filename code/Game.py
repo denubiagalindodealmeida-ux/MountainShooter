81332240f09420adcf4aee5556bab288dd5640b8
Denúbia Almeida <denubiagalindodealmeida@gmail.com>
2026-05-12 00:14:39 -0300

    cerat main loop connecting Menu and Game classes

--- code/Game.py
+++ code/Game.py
@@ -4,13 +4,14 @@ from idlelib import window
 
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH , WIN_HEIGHT))
 
     def run(self):
 
@@ -20,8 +21,3 @@ class Game:
             pass
 
 
-            # check for all events
-            # for event in pygame.event.get():
-            #   if event.type == pygame.QUIT:
-            #    pygame.quit()  # close window
-            #    quit()  # end pygame
---
+++ code/const.py
@@ -0,0 +1,16 @@
+from code.Const import WIN_WIDTH, MENU_OPTION
+
+#C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+#M
+MENU_OPTION = ('NEW_GAME 1P',
+               'NEW GAEM 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
